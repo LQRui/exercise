52491fab4b7aa97af921d10670822e798b0e323b
Administrator <onlylqr@163.com>
2018-06-19 22:03:26 +0800

    exercise

---
+++ 类和对象：组合.py
@@ -0,0 +1,16 @@
+class Turtle:
+    def __init__(self,x):
+        self.num = x
+
+class Fish:
+    def __init__(self,x):
+        self.num = x
+
+class Pool :
+    def __init__(self,x, y):
+        self.turtle = Turtle(x)
+        self.fish= Fish(y)
+    def print_num (self):
+        print("水池中总共有乌龟 %d 只，小鱼 %d 条！"%(self.turtle.num,self.fish.num))
+pool = Pool(10,100)
+pool.print_num()
